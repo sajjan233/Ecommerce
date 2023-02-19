40d268e64e88a24fc6dac1e9a64f35d3d4f7fc2b
sajjan <sajjanhsr23@gmail.com>
2023-02-19 20:27:51 +0530

    install some module

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import bodyparser from 'body-parser';
 const app = express();
 
 
-app.lestion(4000,() => {
-    console.log('server run');
+app.listen(4000,() => {
+    console.log('server run on port',4000);
 })
